644e762a055ac831cc771934e6734a39b88e4d0f
Alejandro Vittar <alejandrovittar@gmail.com>
2023-10-06 20:45:59 -0300

    commit del quilombo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('el cambio de Ivana es mas importante');
-//console.log('el primer cambio de julio');
-//console.log('el primer cambio de Mariano');
-console.log('hice un push solo con git push');
+function crearConflicto(){
+    console.log('que quilombo');
+}
+
