e426b516a38c0653c7c4dc80cb30c496e3eea183
ivana <lunaivana01@gmail.com>
2023-10-06 20:26:23 -0300

    commit importante de ivana

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log('el primer cambio de julio');
-console.log('el primer cambio de Mariano');
+console.log('el cambio de Ivana es mas importante');
+//console.log('el primer cambio de julio');
+//console.log('el primer cambio de Mariano');
